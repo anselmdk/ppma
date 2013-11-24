ae35a4b8283174743d5862bdebf656518104cb97
Pierre Klink <dev@klinks.info>
2013-11-24 14:32:05 +0100

    added fix to last commit

--- public/index.php
+++ public/index.php
@@ -2,11 +2,14 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// set mode
-if (!defined('DEV_MODE')) define('DEV_MODE', false);
-
 // load config
 $config = require(__DIR__ . '/../config.php');
+
+// set mode
+if (!defined('DEV_MODE'))
+{
+    define('DEV_MODE', false);
+}
 
 // create and run app
 (new ppma($config))->run();
