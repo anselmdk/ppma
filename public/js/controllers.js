afb82f02e4d3f5b7c4a21a5cc33d4e719dd7d698
Pierre Klink <dev@klinks.info>
2014-12-30 20:56:28 +0100

    added CategoryWidget

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,13 +1,21 @@
 var ppmaControllers = angular.module('ppmaControllers', ['ppmaServices']);
 
+
+ppmaControllers.controller('CategoryWidgetCtrl', ['$scope', 'Category', 'Entry',
+    function ($scope, Category, Entry) {
+        $scope.categories = Category.query(function() {
+            angular.forEach($scope.categories, function(category, index) {
+                var entries = Entry.query({ categoryId: category.id }, function () {
+                    $scope.categories[index].entryCount = entries.length;
+                });
+            });
+        });
+    }]);
+
+
 ppmaControllers.controller('EntryListCtrl', ['$scope', 'Entry',
     function ($scope, Entry) {
         $scope.entries = Entry.query();
-    }]);
-
-ppmaControllers.controller('EntryViewCtrl', ['$scope', 'Entry', '$routeParams',
-    function ($scope, Entry, $routeParams) {
-        $scope.entry = Entry.get({entryId: $routeParams.id});
     }]);
 
 ppmaControllers.controller('EntryUpdateCtrl', ['$scope', 'Entry', '$routeParams', '$location',
@@ -18,4 +26,9 @@ ppmaControllers.controller('EntryUpdateCtrl', ['$scope', 'Entry', '$routeParams'
             $scope.entry.$save();
             $location.url('/entries');
         }
+    }]);
+
+ppmaControllers.controller('EntryViewCtrl', ['$scope', 'Entry', '$routeParams',
+    function ($scope, Entry, $routeParams) {
+        $scope.entry = Entry.get({entryId: $routeParams.id});
     }]);
--- public/js/services.js
+++ public/js/services.js
@@ -1,7 +1,9 @@
 var ppmaServices = angular.module('ppmaServices', ['ngResource']);
 
+ppmaServices.factory('Category', ['$resource', function($resource) {
+    return $resource('/categories/:categoryId');
+}]);
+
 ppmaServices.factory('Entry', ['$resource', function($resource) {
-    return $resource('/entries/:entryId', {}, {
-        query: { method: 'GET', isArray: true }
-    });
+    return $resource('/entries/:entryId');
 }]);
